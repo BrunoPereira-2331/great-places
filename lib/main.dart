bfc08fae343ee78e4b1c3c9212d3c8c1545d1c87
Bruno Pereira <bruno.pereira@brudam.com.br>
2023-01-23 22:22:21 -0300

    mellowship

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/place_detail_screen.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const App());
+}
+
+setSharedPreferencesData() async {
+  await DotEnv().load(fileName: '.env');
+  Future<SharedPreferences> shared = SharedPreferences.getInstance();
+  shared.then((sharedData) {
+    String googleMapsApiKey = DotEnv().env['GOOGLE_MAPS_API_KEY'] ?? "";
+    sharedData.setString("google_maps_api_key", googleMapsApiKey);
+  });
 }
 
 class App extends StatelessWidget {
@@ -15,6 +26,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    setSharedPreferencesData();
+
     final ThemeData theme = ThemeData();
     return ChangeNotifierProvider(
       create: (ctx) => GreatPlaces(),
--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -2,21 +2,28 @@ import 'dart:convert';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-const GOOGLE_API_KEY = '';
+Future<String> getGoogleApiKey() async {
+  Future<SharedPreferences> shared = SharedPreferences.getInstance();
+  String googleMapsApiKey = "";
+  await shared.then((sharedData) => googleMapsApiKey = sharedData.getString("google_maps_api_key")!);
+  return googleMapsApiKey;
+}
 
 class LocationUtil {
   static String generateLocationPreviewImage({
     required double latitude,
     required double longitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=${getGoogleApiKey()}';
   }
 
-  static Future<String>getAddressFrom(LatLng position) async {
-    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$GOOGLE_API_KEY';
+  static Future<String> getAddressFrom(LatLng position) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=${getGoogleApiKey()}';
     final response = await http.get(Uri.parse(url));
     return jsonDecode(response.body)['results'][0]['formatted_address'];
-
   }
 }
